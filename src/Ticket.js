80038a94e9fb8e1fc5773539de9b3365a6953d3a
Andy Read <andyreadpnw@gmail.com>
2020-01-15 06:47:10 -0800

    Data Tables Functional

--- src/Ticket.js
+++ src/Ticket.js
@@ -1,80 +1,147 @@
 import React, { Component } from "react";
-import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
+import ReactTable from "react-table";
+import DataTable from "react-data-table-component";
 
 export class Ticket extends Component {
+  constructor(props) {
+    super(props);
+  }
+  // handleClick(e) {
+  //   e.preventDefault();
+  //   console.log("The link was clicked.");
+  // }
+
+  handleClick = () => {
+    console.log("potato");
+  };
+
+  handleRowClicked = row => {
+    console.log(`${row.id} was clicked!`);
+  };
+
   render() {
-    let {
-      id,
-      parent_id,
-      product_name,
-      style_id,
-      department,
-      status,
-      priority,
-      issue_type,
-      issue_class,
-      site,
-      division,
-      environment,
-      log_body,
-      assigned,
-      cc
-    } = this.props.data;
     console.log(this.props.data);
-    let products = [
+
+    const Button = () => <button type="button">Edit</button>;
+
+    const data = this.props.data;
+
+    const columns = [
       {
-        id: id,
-        parent_id: parent_id,
-        product_name: product_name,
-        style_id: style_id,
-        department: department,
-        status: status,
-        priority: priority,
-        issue_type: issue_type,
-        issue_class: issue_class,
-        site: site,
-        division: division,
-        environment: environment,
-        log_body: log_body,
-        assigned: assigned,
-        cc: cc
+        name: "ID",
+        selector: "id",
+        sortable: true,
+        width: "40px"
+      },
+      {
+        name: "Status",
+        selector: "status",
+        sortable: true,
+        right: true,
+        width: "90px"
+      },
+      {
+        name: "Priority",
+        selector: "priority",
+        sortable: true,
+        right: true,
+        width: "90px"
+      },
+      {
+        name: "Issue Type",
+        selector: "issue_type",
+        sortable: true,
+        right: true,
+        width: "120px"
+      },
+      {
+        name: "Issue Class",
+        selector: "issue_class",
+        sortable: true,
+        right: true,
+        width: "140px"
+      },
+      {
+        name: "Site",
+        selector: "site",
+        sortable: true,
+        right: true,
+        width: "90px"
+      },
+      {
+        name: "Division",
+        selector: "division",
+        sortable: true,
+        right: true,
+        width: "90px"
+      },
+      {
+        name: "Environment",
+        selector: "environment",
+        sortable: true,
+        right: true,
+        width: "90px"
+      },
+      {
+        name: "Action Request",
+        selector: "log_body",
+        sortable: true,
+        right: true,
+        grow: 120
+      },
+      {
+        name: "Assigned",
+        selector: "assigned",
+        sortable: true,
+        right: true,
+        width: "120px"
+      },
+      {
+        name: "CC",
+        selector: "cc",
+        sortable: true,
+        right: true
+      },
+      {
+        name: "Date Prediction",
+        selector: "date_due",
+        sortable: true,
+        right: true
       }
     ];
+
+    const customStyles = {
+      rows: {
+        style: {
+          minHeight: "72px"
+        }
+      },
+      headCells: {
+        style: {
+          paddingLeft: "8px",
+          paddingRight: "8px"
+        }
+      },
+      cells: {
+        style: {
+          paddingLeft: "8px",
+          paddingRight: "8px"
+        }
+      }
+    };
+
     return (
       <div>
         {/* {id} */}
-        <BootstrapTable data={this.props.data}>
-          <TableHeaderColumn dataField="id" isKey>
-            Product ID
-          </TableHeaderColumn>
-          <TableHeaderColumn dataField="parent_id">parent_id</TableHeaderColumn>
-          <TableHeaderColumn dataField="product_name">
-            product_name
-          </TableHeaderColumn>
-          <TableHeaderColumn dataField="style_id">style_id</TableHeaderColumn>
-          <TableHeaderColumn dataField="department">
-            department
-          </TableHeaderColumn>
-          <TableHeaderColumn dataField="status">status</TableHeaderColumn>
-          <TableHeaderColumn dataField="priority">priority</TableHeaderColumn>
-          <TableHeaderColumn dataField="issue_type">
-            issue_type
-          </TableHeaderColumn>
-          <TableHeaderColumn dataField="issue_type">
-            issue_type
-          </TableHeaderColumn>
-          <TableHeaderColumn dataField="issue_class">
-            issue_class
-          </TableHeaderColumn>
-          <TableHeaderColumn dataField="site">site</TableHeaderColumn>
-          <TableHeaderColumn dataField="division">division</TableHeaderColumn>
-          <TableHeaderColumn dataField="environment">
-            environment
-          </TableHeaderColumn>
-          <TableHeaderColumn dataField="log_body">log_body</TableHeaderColumn>
-          <TableHeaderColumn dataField="assigned">assigned</TableHeaderColumn>
-          <TableHeaderColumn dataField="cc">cc</TableHeaderColumn>
-        </BootstrapTable>
+        {/* <ReactTable data={this.props.data} columns={columns} /> */}
+        <DataTable
+          title="Product Logs"
+          columns={columns}
+          data={data}
+          onRowClicked={this.handleRowClicked}
+          onClick={this.handleClick}
+          pagination
+        />
       </div>
     );
   }
