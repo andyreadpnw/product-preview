84d2c13ca78ef6261c049e9048a532785c775ec4
Andy Read <andyreadpnw@gmail.com>
2020-01-18 15:05:41 -0800

    csv parsing

--- src/Profile.js
+++ src/Profile.js
@@ -1,8 +1,36 @@
 import React, { Component } from "react";
+import { CSVReader } from "react-papaparse";
 
 export class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.fileInput = React.createRef();
+  }
+
+  handleReadCSV = data => {
+    console.log(data);
+  };
+
+  handleOnError = (err, file, inputElem, reason) => {
+    console.log(err);
+  };
+
+  handleImportOffer = () => {
+    this.fileInput.current.click();
+  };
+
   render() {
-    return <div>hi</div>;
+    return (
+      <div>
+        <CSVReader
+          onFileLoaded={this.handleReadCSV}
+          inputRef={this.fileInput}
+          style={{ display: "none" }}
+          onError={this.handleOnError}
+        />
+        <button onClick={this.handleImportOffer}>Import</button>
+      </div>
+    );
   }
 }
 
