8108b4a59ef51c25a252709a8c5b77f5136c1b51
Andy Read <andyreadpnw@gmail.com>
2020-01-29 14:43:19 -0800

    approvals table

--- src/PDP.js
+++ src/PDP.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Table } from "reactstrap";
 
 export class PDP extends Component {
   render() {
@@ -17,7 +18,7 @@ export class PDP extends Component {
     let {
       ecomm_approve,
       ecomm_approver,
-      ecom_comment,
+      ecomm_comment,
       plm_approve,
       plm_approver,
       plm_comment,
@@ -31,6 +32,7 @@ export class PDP extends Component {
       other_approver,
       other_comment
     } = this.props.approvals[0];
+    console.log(this.props.approvals);
     return (
       <div>
         <header>
@@ -59,12 +61,49 @@ export class PDP extends Component {
               </details>
             </section>
           </div>
-          <div class="grid-item">
-            Ecomm PLM Merchant Planner Other {ecomm_approve},{ecomm_approver}
-            {ecom_comment},{plm_approve},{plm_approver},{plm_comment},
-            {merchant_approve},{merchant_approver},{merchant_comment},
-            {planner_approve},{planner_approver},{planner_comment},
-            {other_approve},{other_approver},{other_comment}
+          <div class="grid-item2">
+            <Table striped>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Approve</th>
+                  <th>Approver</th>
+                  <th>Comments</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">Ecomm</th>
+                  <td>{ecomm_approve}</td>
+                  <td>{ecomm_approver}</td>
+                  <td>{ecomm_comment}</td>
+                </tr>
+                <tr>
+                  <th scope="row">PLM</th>
+                  <td>{plm_approve}</td>
+                  <td>{plm_approver}</td>
+                  <td>{plm_comment}</td>
+                </tr>
+                <tr>
+                  <th scope="row">Merchant</th>
+                  <td>{merchant_approve}</td>
+                  <td>{merchant_approver}</td>
+                  <td>{merchant_comment}</td>
+                </tr>
+                <tr>
+                  <th scope="row">Planner</th>
+                  <td>{planner_approve}</td>
+                  <td>{planner_approver}</td>
+                  <td>{planner_comment}</td>
+                </tr>
+                <tr>
+                  <th scope="row">Other</th>
+                  <td>{other_approve}</td>
+                  <td>{other_approver}</td>
+                  <td>{other_comment}</td>
+                </tr>
+              </tbody>
+            </Table>
           </div>
         </div>
       </div>
